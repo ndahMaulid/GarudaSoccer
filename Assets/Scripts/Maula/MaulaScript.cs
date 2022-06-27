859c0689981c05723d6c1e327ab1d1a8b3207cd1
Atqa Munzir <atqamz@gmail.com>
2022-06-27 10:58:05 +0700

    Benerin Mekanik Lari

---
+++ Assets/Scripts/Maula/MaulaScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MaulaScript : MonoBehaviour
+{
+  public float jumpForce = 10f;
+  public float respawnDelay = 1f;
+
+  [SerializeField] bool isGrounded = false;
+
+  Rigidbody2D rb;
+  Transform ball;
+
+  void Awake()
+  {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  void Update()
+  {
+  }
+  void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.CompareTag("ground"))
+    {
+      if (!isGrounded)
+      {
+        isGrounded = true;
+      }
+    }
+
+    if (collision.gameObject.CompareTag("obstackle"))
+    {
+      GetComponent<Health>().TakeDamage(1);
+      StartCoroutine(Respawn(respawnDelay));
+    }
+  }
+  public void Jump()
+  {
+    if (isGrounded)
+    {
+      rb.AddForce(Vector3.up * jumpForce);
+      isGrounded = false;
+    }
+  }
+
+  IEnumerator Respawn(float delay)
+  {
+    yield return new WaitForSeconds(delay);
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+}
