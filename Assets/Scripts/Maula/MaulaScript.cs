71be4eae769d3a84f308b5c6880d2115eecd9acc
Atqa Munzir <atqamz@gmail.com>
2022-06-27 20:56:20 +0700

    Hampir Jadi

--- Assets/Scripts/Maula/MaulaScript.cs
+++ Assets/Scripts/Maula/MaulaScript.cs
@@ -6,35 +6,77 @@ using UnityEngine.SceneManagement;
 public class MaulaScript : MonoBehaviour
 {
   public bool canJump;
-  [SerializeField] float jumpAmount = 10f;
+  public float jumpAmount = 10f;
+  public float totalCoin = 0f;
+  [SerializeField] float runAmount = 2f;
+  [SerializeField] float respawnTime = 3f;
+  [SerializeField] float coinValue = 50f;
   Health health;
+
+  Rigidbody2D rb;
+  Animator animator;
+  [SerializeField] GameObject WinCanvas;
 
   void Awake()
   {
     health = FindObjectOfType<Health>();
+    rb = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
   }
 
+  void FixedUpdate()
+  {
+    Run();
+  }
 
   void OnCollisionEnter2D(Collision2D other)
   {
     if (other.gameObject.CompareTag("ground"))
     {
       canJump = true;
+      animator.SetBool("isJumping", false);
     }
 
     if (other.gameObject.CompareTag("obstackle"))
     {
       health.TakeDamage(1);
-      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+      StartCoroutine(Respawn());
     }
   }
 
-  void Jump()
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.gameObject.CompareTag("Finish"))
+    {
+      WinCanvas.SetActive(true);
+      Time.timeScale = 0;
+    }
+
+    if (other.gameObject.CompareTag("coin"))
+    {
+      totalCoin += coinValue;
+      Destroy(other.gameObject);
+    }
+  }
+
+  void Run()
+  {
+    rb.velocity = new Vector2(runAmount, rb.velocity.y);
+  }
+
+  public void Jump()
   {
     if (canJump)
     {
-      GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpAmount);
+      rb.velocity = new Vector2(rb.velocity.x, jumpAmount);
+      animator.SetBool("isJumping", true);
       canJump = false;
     }
+  }
+
+  IEnumerator Respawn()
+  {
+    yield return new WaitForSeconds(respawnTime);
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
   }
 }
---
+++ Assets/Scripts/Trick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trick : MonoBehaviour
+{
+  [SerializeField] Button trickButton;
+  GameObject maula;
+
+  void Awake()
+  {
+    maula = GameObject.FindGameObjectWithTag("Player");
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.gameObject.CompareTag("Player"))
+    {
+      trickButton.interactable = true;
+    }
+  }
+
+  public void PlayTrick()
+  {
+    maula.GetComponent<Rigidbody2D>().velocity = new Vector2(maula.GetComponent<Rigidbody2D>().velocity.x, maula.GetComponent<MaulaScript>().jumpAmount);
+    maula.GetComponentInParent<Animator>().SetTrigger("isTricking");
+    trickButton.interactable = false;
+    Destroy(gameObject);
+  }
+}
